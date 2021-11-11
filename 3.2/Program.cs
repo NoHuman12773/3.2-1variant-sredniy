6c3f12542daef5dab8af716f735ff209aacee85e
tihon <tihon@DESKTOP-ACP6SLU>
2021-11-11 22:09:35 +0200

    Добавьте файлы проекта.

---
+++ 3.2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double a = 1,d=0,S;
+                do
+                {
+                    d = 2.54;
+                    S = a * d;
+                    Console.WriteLine("|{0}|{1}|", a, S);
+                    a ++;
+                } while (a!=21);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
